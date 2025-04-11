0f21036f92b7eaa7d3acbd47d44ddd0d61290745
Jacob Berger <jacob@bergerj.com>
2025-04-11 13:44:08 -0700

    lab 1 complete

--- PythonIntro/python_intro.py
+++ PythonIntro/python_intro.py
@@ -50,7 +50,7 @@ def backward(my_string):
         >>> backward("ipython")
         'nohtypi'
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    return my_string[::-1]
 
 
 # Problem 5
@@ -69,7 +69,15 @@ def list_ops():
         >>> list_ops()
         ['fox', 'hawk', 'dog', 'bearhunter']
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    list = ["bear", "ant", "cat", "dog"]
+    list.append("eagle")
+    list[2] = "fox"
+    list.pop(1)
+    list.sort(reverse=True)
+    list[list.index("eagle")] = "hawk"
+    list[-1] += "hunter"
+
+    return list
 
 
 # Problem 6
@@ -82,7 +90,14 @@ def pig_latin(word):
         >>> pig_latin("banana")
         'ananabay'
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    vowels = {'a','e','i','o','u'}
+
+    if len(word) == 0:
+        return ''
+    if word[0] in vowels:
+        return word + "hay"
+    else:
+        return word[1:] + word[0] + "ay"
 
 
 # Problem 7
@@ -90,17 +105,35 @@ def palindrome():
     """ Find and retun the largest panindromic number made from the product
     of two 3-digit numbers.
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    best_num = 0
+
+    for num1 in range(100,1000):
+        for num2 in range(100,1000):
+            prod = num1 * num2
+            is_palindrome = str(prod) == str(prod)[::-1]
+            is_best = prod > best_num
+            if (is_palindrome and is_best):
+                best_num = prod
+    return best_num
 
 # Problem 8
 def alt_harmonic(n):
     """ Return the partial sum of the first n terms of the alternating
     harmonic series, which approximates ln(2).
     """
-    raise NotImplementedError("Problem 8 Incomplete")
+    list = [(-1)**(i+1) / i for i in range(1, n+1)]
+    print(list)
+    return sum(list)
 
 
 if __name__ == "__main__":
     print(sphere_volume(2))
     isolate(1,2,3,4,5)
     print(first_half("python"))
+    print(backward("python"))
+    print(list_ops())
+    print(pig_latin("apple"))
+    print(pig_latin("banana"))
+    print(pig_latin(""))
+    print(palindrome())
+    print(alt_harmonic(500000))
